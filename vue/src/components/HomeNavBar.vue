8d099358ff5701ce381fcf2c68c719389428329e
Uriel Martinez <martinezu444@gmail.com>
2023-12-05 17:42:51 -0500

    HomeView update

---
+++ vue/src/components/HomeNavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <span class="icon-text">
+                <span class="icon">
+                    <i class="fa-regular fa-graduation-cap" width="112" height="28"></i>
+                </span>
+                <span>LEARN WELL</span>
+            </span>    
+        </div>
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item">
+                    Home
+                </a>
+            </div>
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-link is-outlined">
+                        <strong>Register</strong>
+                    </a>
+                    <a class="button is-link">
+                        Log In
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</template>
+
+<script></script>
+
+<style></style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="home">
+    <home-nav-bar></home-nav-bar>
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
 </template>
 
 <script>
+import HomeNavBar from '../components/HomeNavBar.vue'
 export default {
+  components: {
+    HomeNavBar,
+  }
 };
 </script>
