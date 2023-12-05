e66f3cb6a9699cc52c6cfef54d06686b350aabf6
Ryan Glod <Polymathryan@gmail.com>
2023-12-05 18:03:14 -0500

    MVC Controller Get Methods

---
+++ dotnet/Capstone/Controllers/AssignmentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    //not using this as of right now
+    public class AssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
