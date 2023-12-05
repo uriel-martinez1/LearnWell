63dc964d69c9a03e2595897730b10586c024cab5
Ifeyinwa Uwazie <icu2065@gmail.com>
2023-12-05 18:58:31 -0500

    RegistrationView beautification work in progress

--- vue/src/views/RegisterView.vue
+++ vue/src/views/RegisterView.vue
@@ -1,50 +1,66 @@
 <template>
-  <div id="register" class="text-center">
-    <form v-on:submit.prevent="register">
+  <div id="app" class="text-center">
+    <form class="box" v-on:submit.prevent="register">
       <h1>Create Account</h1>
       <div role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
       </div>
       <div class="field">
         <label for="first-name"></label>
-        <input class="input" type="text" id="first-name" placeholder="First Name" v-model="user.firstName" required
+        <div class="control has-icons-left">
+        <input class="input is-success" type="text" id="first-name" placeholder="First Name" v-model="user.firstName" required
           autofocus />
+          <span class="icon is-small is-left">
+            <i class="fa-regular fa-user"></i>
+          </span>
       </div>
-      <div class="field">
+    </div>
+
+    <div class="field">
         <label for="last-name"></label>
-        <input class="input" type="text" id="last-name" placeholder="Last Name" v-model="user.lastName" required
+        <div class="control has-icons-left">
+        <input class="input is-success" type="text" id="last-name" placeholder="Last Name" v-model="user.lastName" required
           autofocus />
+          <span class="icon is-small is-left">
+            <i class="fa-regular fa-user"></i>
+          </span>
       </div>
+    </div>
+      
       <div class="field">
         <label for="email"></label>
-        <input class="input is-success" type="email" id="email" placeholder="Email Address" v-model="user.email" required
-          autofocus />
-        <span class="icon is-small is-left">
-          <i class="fas fa-envelope"></i>
-        </span>
+        <div class="control has-icons-left">
+          <input class="input is-success" type="email" id="email" placeholder="Email Address" v-model="user.email"
+            required autofocus />
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+        </div>
       </div>
       <div class="field">
         <label for="username"></label>
-        <input class="input is-success" type="text" id="username" placeholder="Create your username"
-          v-model="user.username" required autofocus />
-        <span class="icon is-small is-left">
-          <i class="fas fa-user"></i>
-        </span>
+        <div class="control has-icons-left">
+          <input class="input is-success" type="text" id="username" placeholder="Create your username"
+            v-model="user.username" required autofocus />
+          <span class="icon is-small is-left">
+            <i class="fas fa-user"></i>
+          </span>
+        </div>
       </div>
       <div class="field">
         <label for="password"></label>
+
         <input type="password" id="password" placeholder="Create your password" v-model="user.password" required />
-        <span class="icon is-small is-left">
-          <i class="fas fa-key"></i>
-        </span>
+
+
       </div>
       <div class="field">
         <label for="confirmPassword"></label>
+
         <input type="password" id="confirmPassword" placeholder="Confirm your password" v-model="user.confirmPassword"
           required />
-        <span class="icon is-small is-left">
-          <i class="fas fa-key"></i>
-        </span>
+
+
       </div>
       <input name="isTeacher" type="radio" id="teacher-yes" v-model="user.teacher" v-bind:value="true" />
       <label for="teacher-yes">Teacher</label>
@@ -53,14 +69,21 @@
       <div v-show="user.teacher" class="field">
         <label class="label"></label>
         <div class="control">
-          <input class="input" type="text" placeholder="Enter your teacher key">
+          <div class="control has-icons-right">
+            <input class="input" type="text" placeholder="Enter your teacher key">
+            <span class="icon is-small is-right">
+              <i class="fas fa-key"></i>
+            </span>
+          </div>
         </div>
       </div>
-      <button class="button is-link" type="submit">Register Account</button>
+      <button class="button is-link is-outlined" type="submit">Register Account</button>
       <p><router-link v-bind:to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
     </form>
   </div>
 </template>
+
+
 
 <script>
 import authService from '../services/AuthService';
@@ -132,4 +155,23 @@ export default {
 label {
   margin-right: 0.5rem;
 }
+
+#app {
+  margin-left: 10px;
+}
+
+.field {
+  width: 50%;
+}
+
+  .registration-form { 
+    max-width: 400px; 
+  margin: 0 auto; 
+  background-color: #fff; 
+  padding: 20px; 
+  border-radius: 8px; 
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); 
+}
+   .error-message { color: #ff3860; margin-bottom: 10px; } 
+  
 </style>
