44713c8f2cae83e8e5fa73a17bf8fecf7478610d
Isaac Saunders <IsaacLSaunders@gmail.com>
2023-12-06 09:54:36 -0500

    ILS: course dao

--- dotnet/Capstone/DAO/CourseSqlDao.cs
+++ dotnet/Capstone/DAO/CourseSqlDao.cs
@@ -2,5 +2,13 @@
 {
     public class CourseSqlDao
     {
+
+        private readonly string connectionString;
+
+        public CourseSqlDao(string connString)
+        {
+            connectionString = connString;
+        }
+
     }
 }
