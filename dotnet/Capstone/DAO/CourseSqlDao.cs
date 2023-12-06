ad7ca7f8e13366383c949f97c072f535e7d3b79a
Ryan Glod <Polymathryan@gmail.com>
2023-12-06 16:57:01 -0500

    Dependency Injection

--- dotnet/Capstone/DAO/CourseSqlDao.cs
+++ dotnet/Capstone/DAO/CourseSqlDao.cs
@@ -1,4 +1,5 @@
-﻿using Capstone.Exceptions;
+﻿using Capstone.DAO.SqlDaoInterfaces;
+using Capstone.Exceptions;
 using Capstone.Models;
 using System;
 using System.Collections.Generic;
@@ -6,7 +7,7 @@ using System.Data.SqlClient;
 
 namespace Capstone.DAO
 {
-    public class CourseSqlDao
+    public class CourseSqlDao : ICourseDao
     {
 
         private readonly string connectionString;
