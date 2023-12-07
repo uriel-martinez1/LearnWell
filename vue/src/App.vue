0c8df8b7defff4b6976b864b890da20a3299013f
Ifeyinwa Uwazie <icu2065@gmail.com>
2023-12-07 16:07:07 -0500

    Removed FormComponents from LoginView

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div id="capstone-app">
     <div id="nav">
-      <!--<router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;-->
-      <nav class="navbar" role="navigation" aria-label="main navigation"></nav>
-  <div class="navbar-brand">
-    <a class="navbar-item" href="http://127.0.0.1:5173/login">
-      <!-- <img src="our logo link here" alt="" 
-      width="112" height="28"> -->
-    </a>
-
-    
-  </div>
-      
+            
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
