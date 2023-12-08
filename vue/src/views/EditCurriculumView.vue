6efeadf6a6a22f3bad1e61919849e88a521e7cb6
Uriel Martinez <martinezu444@gmail.com>
2023-12-08 14:11:09 -0500

    Adding the create and edit feature for the curriculum view

---
+++ vue/src/views/EditCurriculumView.vue
@@ -0,0 +1,28 @@
+<template>
+    <create-curriculum v-bind:content="this.content"></create-curriculum>
+</template>
+
+<script>
+import CreateCurriculum from '../components/CreateCurriculum.vue'
+import TeacherService from '../services/TeacherService';
+
+export default {
+    data(){
+        return{
+            content :{}
+        }
+    },
+    components: {
+        CreateCurriculum,
+    },
+    created(){
+        TeacherService.getCurriculumElementById(this.$route.params.elementId)
+            .then((response) => {
+                console.log(response.data, "INSIDE CREATED getting CURRICULUM ELEMENT")
+                this.content = response.data
+            })
+    }
+}
+</script>
+
+<style></style>
