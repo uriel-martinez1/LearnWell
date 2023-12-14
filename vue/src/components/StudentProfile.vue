b9d472ada4d754e7318ef030a12379db1a09a18b
Uriel Martinez <martinezu444@gmail.com>
2023-12-14 13:45:30 -0500

    Latest iteration of the profile pic

--- vue/src/components/StudentProfile.vue
+++ vue/src/components/StudentProfile.vue
@@ -1,38 +1,35 @@
 <template>
-    <!--We need to grad the student information based on the course?-->
-    <div class="box has-background-white-ter">
-        
+    <nav class="breadcrumb is-medium is-centered" aria-label="breadcrumbs">
+        <ul>
+            <li><a v-on:click="backToDashboard">Dashboard</a></li>
+            <li class="is-active"><a href="#" aria-current="page">{{ student.firstName }} {{ student.lastName }}</a></li>
+        </ul>
+    </nav>
+    <div class="box">
         <section class="studentProfile">
-            <h1 class="title is-5 has-text-centered has-text-black">Student Profile</h1>
+            <h1 class="is-size-1"><strong>Student Profile</strong></h1>
         </section>
-        <div class="columns">
-            <div class="column is-two-thirds">
-                <h2>
-                    <span class="icon is-small">
-                        <i class="fas fa-user"></i>
-                    </span>
-                    {{ student.firstName }} {{ student.lastName }}
-                </h2>
-                <p>
-                    <span class="icon is-small">
-                        <i class="fas fa-envelope"></i>
-                    </span>
-                    {{ student.email }}
-                </p>
-                <progress class="progress is-success" value="60" max="100">60%</progress>
+        
+            <div class="columns">
+                <div class="column is-one-third has-text-centered">
+                    <!-- Add your image source dynamically or use a placeholder -->
+                    <img src="../icon/icons8-username-96.png" alt="Profile" class="square-image">
+                </div>
+                <div class="column is-two-thirds has-text-centered">
+                    <h2 class="is-size-2">
+                        {{ student.firstName }} {{ student.lastName }}
+                    </h2>
+                    <p class="is-size-5">
+                        <span class="icon is-small">
+                            <i class="fas fa-envelope"></i>
+                        </span>
+                         {{ student.email }}
+                    </p>
+                    <!-- <progress class="progress is-link" value="60" max="100">60%</progress> -->
+                </div>
             </div>
-            <div class="column is-one-third has-text-centered">
-                <!-- Add your image source dynamically or use a placeholder -->
-                <img src="https://bulma.io/images/placeholders/128x128.png" alt="Profile" class="square-image">
-            </div>
-        </div>
+        
     </div>
-
-    <!--First we need to grab the curriculum data-->
-
-    <!--Next we need t grab the assignments-->
-
-    <!--Then we grab their submitted assignments-->
 </template>
 
 <script>
@@ -53,21 +50,37 @@ export default {
             .then((response) => {
                 this.student = response.data
             })
+    },
+    methods: {
+        backToDashboard() {
+            this.$router.push({ name: 'TeacherDashboardView', params: { 'id': this.$store.state.user.userId } });
+        }
     }
 }
 </script>
 
-<style >
+<style>
+.box {
+    max-width: 60%;
+    margin-left: 20%;
+
+}
+
 .studentProfile {
     display: flex;
     justify-content: space-between;
     align-items: center;
     margin-bottom: 30px;
-    padding-top: 30px;
+    padding-top: 10px;
 }
+
 
 .content {
     position: relative;
     left: 0px;
+}
+
+.is-active:hover {
+    cursor: default;
 }
 </style>
